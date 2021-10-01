dc14e48bfb66d32238c5e765c0b6a5fd09fa86fa
Thrawn1 <b.eremin@gmail.com>
2021-10-01 10:41:40 +0300

    Update id_employer.py
    Исправлена ошибка кодировки для работы в windows

--- id_employer.py
+++ id_employer.py
@@ -8,7 +8,7 @@ def id_employer(name_id_file = 'id_employer_new_v2.dat',name_roles_file = 'roles
         Так же принимает параметр, который определяет, что функция отдает - простой список, или структуру по ролям.
     """
     if type_data == 1:
-        file_id_name = open(name_id_file, 'r')
+        file_id_name = open(name_id_file, 'r',encoding='utf-8')
         id_employer_list = {}
         for line in file_id_name:
             new_line = line.rstrip('\n')
@@ -20,13 +20,13 @@ def id_employer(name_id_file = 'id_employer_new_v2.dat',name_roles_file = 'roles
     else:
         id_employer_list = []
         number_roles = 0
-        roles_file = open(name_roles_file,'r')
+        roles_file = open(name_roles_file,'r',encoding='utf-8')
         for line in roles_file:
             number_roles += 1 
         for role in range(1,(number_roles+1)):
             role_dict = {}
             id_employer_list.append(role_dict)
-        file_id_name = open(name_id_file, 'r')
+        file_id_name = open(name_id_file, 'r',encoding='utf-8')
         for line in file_id_name:
             new_line = line.rstrip('\n')
             line_data=new_line.split(' ')
