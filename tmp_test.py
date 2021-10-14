f9e4d3e87726c9d4b5e36d0f2c590835da2fdacc
veremin <trawn@home.su>
2021-10-14 20:32:59 +0300

    Добавлена блок схема

--- tmp_test.py
+++ tmp_test.py
@@ -4,6 +4,7 @@ from read_file_data import*
 from datetime import datetime
 from analyze_data import*
 from calendar import calendar, weekday, monthrange
+from search_missed_day import*
 import random
 import calendar
 file_name = '1_attlog.dat'
@@ -18,7 +19,7 @@ data_array = build_data_array(list_data)
 all_day_month = list(range(1,(int(monthrange(requested_year,requested_month)[1])+1),1))
 search_for_missed_day(data_array,all_day_month,requested_year,requested_month)
 last_day_month = monthrange(requested_year,requested_month)[1]
-search_missing_mark(data_array,last_day_month,requested_year,requested_month)
+search_missing_mark_employee(data_array,last_day_month,requested_year,requested_month)
 #analyze_employee_work_date(data_array)
 print('Готово!')
 # last_day_month = calendar.monthrange(requested_year,requested_month)[1]
