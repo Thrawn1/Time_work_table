cfd0d9a2c91695e37fc98edb5bc16ee5f2e058d9
veremin <veremin@example.com>
2021-10-24 01:16:31 +0300

    Создание функции расчета рабочего времени и расчета переработки

--- calculation_of_hours_and_wages.py
+++ calculation_of_hours_and_wages.py
@@ -1,7 +1,35 @@
-def calculation_of_working_hours(time_table:dict):
-    """Функция для рассчет отработанных часов и переработки. Функция принимает отредактированную структуру данных."""
-    pass
+from id_employee import id_employee
+from datetime import datetime,timedelta
 
+def calculation_of_working_hours(time_table:dict):
+    """Функция для рассчета отработанных часов и переработки. Функция принимает отредактированную структуру данных."""
+    list_employee = id_employee(type_data=2)
+    for cell_date in time_table:
+        print(cell_date)
+        for cell_id in time_table[cell_date]:
+            if cell_id in list_employee[1]:
+                print(cell_id)
+                time_delta_raw = time_table[cell_date][cell_id][0] - time_table[cell_date][cell_id][1]
+                time_delta_sec = time_delta_raw.total_seconds()
+                print(time_delta_raw)
+                work_time = timedelta(hours = 8)
+                yyyy = time_delta_raw - work_time
+                zzzz = timedelta(seconds = 1)
+                uuu = abs(yyyy)
+                if yyyy > zzzz:
+                    print('TEST')
+                    print(time_table[cell_date][cell_id][0])
+                    print(time_table[cell_date][cell_id][1])
+                    print(uuu)
+                else:
+                    print('TEST_22')
+                    print(time_table[cell_date][cell_id][0])
+                    print(time_table[cell_date][cell_id][1])
+                    print(time_delta_raw)
+                    print('-',uuu)
+
+    
 
 def calculation_wages(time_table:dict):
+    """Функция для рассчета заработной платы. Функция принимает отредактированную структуру данных."""
     pass
--- main_tmp.py
+++ main_tmp.py
@@ -1,10 +1,13 @@
 from build_data_array import*
 from id_employee import*
 from analyze_data import*
+from calculation_of_hours_and_wages import*
 
-file_name = '1_attlog.dat'
-requested_year = int(input('Введите год:'))
-requested_month = int(input('Введите месяц:'))
+file_name = '3-2021.dat'
+#requested_year = int(input('Введите год:'))
+#requested_month = int(input('Введите месяц:'))
+requested_year = 2021
+requested_month = 3
 list_data = read_file_data(file_name,requested_year,requested_month)
 data_array = build_data_array(list_data)
 id_list = id_employee(type_data=2)
@@ -17,5 +20,6 @@ for id in id_list[1]:
 for id in id_list[1]:
     analyze_data_for_print(data_array,id,requested_year,requested_month)
 
+calculation_of_working_hours(data_array)
+
 print('Готово!!!')
-print(data_array)
