b253bf6a19db6fdc5688a5c947876dea55a2d697
veremin <veremin@example.com>
2021-10-23 22:59:23 +0300

    Заготовка модуля расчета рабочих часов, переработки и зарплаты

---
+++ calculation_of_hours_and_wages.py
@@ -0,0 +1,7 @@
+def calculation_of_working_hours(time_table:dict):
+    """Функция для рассчет отработанных часов и переработки. Функция принимает отредактированную структуру данных."""
+    pass
+
+
+def calculation_wages(time_table:dict):
+    pass
