2438f2a900bebfee5ce4d92d987eea4916b34e59
Thrawn1 <b.eremin@gmail.com>
2022-11-24 15:13:11 +0300

    Построение исполняемого файла
    Расширен скрипт для построение исполняемого файла.
    Создается директория, в которой находятся необходимые директории и файлы, в том числе и исполняемый. Так же - создается архив, содержащий эту общую директорию. Достаточно скопировать директорию с структурой и исполняемым файлом или распаковать архив. И можно использовать приложение

--- generation_exe.ps1
+++ generation_exe.ps1
@@ -1,2 +1,14 @@
+.\.venv\Scripts\Activate.ps1
 pip install pyinstaller
 pyinstaller --onefile main.py 
+Remove-Item .\build -Recurse -Force
+Remove-Item main.spec -Force
+New-Item -ItemType Directory -Path .\Time_table 
+Copy-Item .\dist\main.exe .\Time_table\Time_table.exe
+New-Item -ItemType Directory -Path .\Time_table\data
+New-Item -ItemType Directory -Path .\Time_table\data\variable_data_for_app
+Copy-Item .\data\variable_data_for_app\* .\Time_table\data\variable_data_for_app
+Copy-Item .\data\1_attlog.dat .\Time_table\data
+Remove-Item .\dist -Recurse -Force
+Compress-Archive -Path .\Time_table -DestinationPath .\Time_table.zip
+Write-Host "DONE! Executable file generated"
