65f8edbbeb3b29e1b60b49628d5be07479484562
Thrawn1 <b.eremin@gmail.com>
2022-12-01 16:06:35 +0300

    Update class_file.py
    Переписан класс Lable, который описывает метку
    Доработан класс work_day, изменен набор атрибутов и переработан конструктор
    Доработан класс  read_row_data

--- class_file.py
+++ class_file.py
@@ -1,92 +1,151 @@
-from datetime import datetime
-from datetime import time as dt_time
+from datetime import date,time
+from datetime import time
 from random import randint
+from os import path
+
+
+class read_file_row_data():
+    """Данный метод необходим, что бы открыть файл с датчика, прочитать данные и сформировать список с данными за месяц. 
+    Данные об отметке хранятся как строка.Данный объект хранит имя файла с данными и путь к файлу."""
+    def __init__(self):
+        self.file_name = '1_attlog.dat'
+        self.directory = 'data' 
+        self.file_data = []
+        self.requset_year = 0
+        self.requset_month = 0
+    def search_work_period(self):
+        """Данный метод ищет в файле данные за определенный месяц и год. Данные хранятся в списке file_data"""
+        with open(path.join(self.directory,self.file_name)) as file:
+            for line in file:
+                if self.detect_data(line[10:17]):
+                    self.file_data.append(line)
+    def detect_data(self,row_date:str):
+        """Данный метод ищет в списке file_data данные за определенный месяц и год. Данные хранятся в списке file_data"""
+        row_year = int(row_date[0:4])
+        row_month = int(row_date[5:])
+        if row_year == self.requset_year and row_month == self.requset_month:
+            return True
+
+
+
+
 class lable():
-    """Данный класс описывает объект, который хранит в себе как атрибут главный флаг - если метка прихода 0, если уход 1. 
-    Кроме того в качестве атрибута хранится строковое значение даты и времени """
+    """Данный класс описывает объект, который хранит в себе информацию о метке с датчика. В качестве атрибутов 
+    класса используются дата, время, флаг, который определяет тип метки(приход или уход), а также строка из файла с датчика,
+     по которой строится объект класса """
+    
     row_data = '' # Переменная для хранения строки из файла
-    flag = 0 # Переменная для хранения флага. Если 0 - приход, если 1 - уход
+    flag = 3 # Переменная для хранения флага. Если 0 - приход, если 1 - уход, 3 - значение по умолчанию, требуется проверка
     date = None # Переменная для хранения даты метки
     time = None # Переменная для хранения времени метки 
     
     def __init__(self, row_data):
         """Данный метод инициализирует объект класса"""
         self.row_data = row_data
-        self.date = row_data[1:17] #Берем срез строки, в котором хранится дата, создаем объект класса datetime и сохраняем в атрибут
-        self.time = row_data[18:24] #Берем срез строки, в котором хранится время, создаем объект класса datetime и сохраняем в атрибут
+        self.date = date.fromisoformat(row_data[10:20]) #Берем срез строки, в котооых хранится дата, создаем объект класса date
+        self.time =  time.fromisoformat(row_data[21:29]) #Берем срез строки, в котором хранится время, создаем объект класса time
+        self.flag = 3 
+   
     def set_flag(self, value):
         """Данный метод устанавливает значение флага"""
         self.flag = value
+   
     def get_flag(self):
         """Данный метод возвращает значение флага"""
         return self.flag
+   
     def get_date(self):
         """Данный метод возвращает значение даты"""
         return self.date
+   
     def get_time(self):
         """Данный метод возвращает значение времени"""
         return self.time
-    def random_time_set(self):
-        pass
- 
+   
+    def random_time_set(self,hour_second_label:int,flag_second_label:int):
+        if flag_second_label == 0:
+            hour = randint(hour_second_label,21)
+        elif flag_second_label == 1:
+            hour = randint(5,hour_second_label)
+        minute = randint(0,59)
+        second = randint(0,59)
+        self.time = time(hour,minute,second)
+
+    def edit_time_lable(self,edit_time:str):
+        """Данный метод изменяет время метки на заданное значение"""
+        self.time = time.fromisoformat(edit_time)
+   
+    def __str__(self) -> str:
+        """Данный метод возвращает строковое представление объекта класса"""
+        if self.flag == 0:
+            flag_str = 'Приход'
+        elif self.flag == 1:
+            flag_str = 'Уход'
+        else:
+            flag_str = 'Не определен'
+        return f'flag: {flag_str}, date: {self.date}, time: {self.time}'
 
 
 
-class read_file_row_data():
-    """Данный метод необходим, что бы открыть файл с датчика, прочитать данные и сформировать список с данными за месяц. Данные об отметке хранятся как объект класса lable 
-    Данный объект хранит в себе год и месяц, за которые нужны данные. Данный объект хранит имя файла с данными и путь к файлу."""
-    def __init__(self, lable):
-        self.lable = lable
-        self.file_name = lable.file_name
-        self.file_path = lable.file_path
-        self.file_data = []
-        self.file_data = self.read_file_data()
-    def read_file_data(self):
-        """Данный метод открывает файл с датчика, читает данные и возвращает список с данными за месяц"""
-        file_data = []
-        with open(self.file_path + self.file_name, 'r') as file:
-            for line in file:
-                file_data.append(line)
-        return file_data
-    def get_file_data(self):
-        """Данный метод возвращает список с данными за месяц"""
-        return self.file_data
-
-
-class day():
+class work_day():
     """Данный класс хранит информацию за день. Атрибутами являются год,месяц,день в числовом виде, а так же день недели, название месяца в строковом виде
      метка прихода, метка ухода """
-    def __init__(self, year, month, day, day_of_week, month_name, come, go):
-        self.year = year
-        self.month = month
-        self.day = day
-        self.day_of_week = day_of_week
-        self.month_name = month_name
-        self.come = come
-        self.go = go
+    year = None
+    month = None
+    day = None
+    day_of_week = None
+    month_name = None
+    lable_come = None
+    lable_go = None
+    
+    def __init__(self, lable_first:lable):
+        months_name = ('Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь')
+        names_weekdays = ('Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье')
+        self.year = lable_first.get_date().year
+        self.month = lable_first.get_date().month
+        self.month_name = months_name[int(lable_first.get_date().month) - 1]
+        self.day = lable_first.get_date().day
+        self.day_of_week = names_weekdays[lable_first.get_date().weekday()]
+        self.come = lable_first
+        self.go = lable_first
+    
     def get_year(self):
         """Данный метод возвращает год"""
         return self.year
+    
     def get_month(self):
         """Данный метод возвращает месяц"""
         return self.month
+    
     def get_day(self):
         """Данный метод возвращает день"""
         return self.day
+    
     def get_day_of_week(self):
         """Данный метод возвращает день недели"""
         return self.day_of_week
+    
     def get_month_name(self):
         """Данный метод возвращает название месяца"""
         return self.month_name
+    
     def get_come(self):
         """Данный метод возвращает метку прихода"""
         return self.come
+    
     def get_go(self):
         """Данный метод возвращает метку ухода"""
         return self.go
 
-class month():
+    def define_loss_lable(self):
+        """Данный метод определяет, есть ли потерянная метка"""
+        if self.come.get_flag() == self.go.get_flag():
+            return True
+        else:
+            return False
+     
+
+class work_month():
     """Данный класс хранит информацию за месяц. Атрибутами являются год,месяц, название месяца в строковом виде, словарь работников, ключи словаря - это if работников, значенеие
     - это список с объектами класса employee, словарь с данными за месяц, где ключом является id работника, а значением объект с данными за день"""
     def __init__(self, year, month, month_name, employees, month_data):
@@ -110,6 +169,7 @@ class month():
     def get_month_data(self):
         """Данный метод возвращает словарь с данными за месяц"""
         return self.month_data
+
 class employee():
     """Данный класс хранит информацию о работнике. Атрибутами являются id работника, фамилия, имя,
     зарплатная ставка,роль, общее рабочее время за месяц"""
@@ -138,9 +198,11 @@ class employee():
     def get_total_work_time(self):
         """Данный метод возвращает общее рабочее время работника"""
         return self.total_work_time
+
 class excel_table():
     """Данный класс хранит информацию для построения таблицы в excel,имя файла, путь до файла. Имеет метод построения excel файла"""
     pass
+
 class html_file():
     """Данный класс хранит информацию для построения html файла, имя файла, путь до файла. Имеет метод построения html файла"""
     pass
