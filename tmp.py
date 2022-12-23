8115a3c09ba9ca24a982fa557343a4a0c7bae481
Thrawn1 <b.eremin@gmail.com>
2022-12-24 01:24:58 +0300

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,20 +1,32 @@
 from datetime import date,time
-from class_file import lable,work_day
+from class_file import lable,work_day,work_month
+import sys
+from os import path
+directory = 'data'
+name_row_file = '1_attlog_b.dat'
+file = open(path.join(directory,name_row_file),'r',encoding='utf-8')
+list_lines = []
+for line in file:
+    list_lines.append(line)
+file.close()
+tmp_line = list_lines[0]
+tmp_line = tmp_line.rstrip('\n')
+year = int(tmp_line[10:14])
+month = int(tmp_line[15:17])
+test_month = work_month(year,month,list_lines)
+test_month.build_day_object()
+print(test_month.work_employees)
+print(test_month.month_data)
+for id in test_month.work_employees:
+    print('\n')
+    print('ID: ',test_month.work_employees[id].id)
+    print('Имя: ',test_month.work_employees[id].name)
+    print('Фамилия: ',test_month.work_employees[id].family)
+    print('Роль: ',test_month.work_employees[id].role_name)
+    print('Ставка: ',test_month.work_employees[id].rate)
 
-str_input = '        8	2021-12-07 05:45:41	1	255	1	0'
-lable_work = lable(str_input)
-work_day_1 = work_day(lable_work)
-print(lable_work)
-print(work_day_1.day_of_week)
-print(work_day_1.define_loss_lable())
-print(work_day_1.get_year(),type(work_day_1.get_year()))
-print(work_day_1.get_month(),type(work_day_1.get_month()))
-print(work_day_1.get_day(),type(work_day_1.get_day()))
-print(work_day_1.lable_come)
-
-print(str_input[10:17])
-test_str = str_input[10:17]
-test_year = int(test_str[0:4])
-test_month = int(test_str[5:7])
-print(test_year,type(test_year))
-print(test_month,type(test_month))
+print(test_month.month_data)
+for id in test_month.month_data:
+    print('ID: ',id)
+    print('Метка прихода: ',test_month.month_data[id][0].get_come())
+    print('Метка ухода: ',test_month.month_data[id][0].get_go())
