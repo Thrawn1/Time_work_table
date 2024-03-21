2b014ec496f0a1e773108665aa1ae6224eec11fa
Thrawn1 <b.eremin@gmail.com>
2024-03-21 22:58:06 +0300

    Update tmp_db.py
    Исправление после анализа линтером

--- tmp_db.py
+++ tmp_db.py
@@ -1,7 +1,9 @@
+import datetime
 import sqlite3
 import os
 import toml
-import datetime
+
+
 
 
 
@@ -11,16 +13,18 @@ def convert_to_iso(date):
         iso_date = date_obj.date().isoformat()
         return iso_date
     except ValueError:
-        print("Неверный формат даты. Пожалуйста, укажите дату в формате ДД.ММ.ГГ.")
+        print("Неверный формат даты. Пожалуйста, укажите дату в формате"
+              "ДД.ММ.ГГ.")
 
 
 def convert_to_ddmmyy(date):
-     try:
+    try:
         date_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
         ddmmyy_date = date_obj.strftime('%d.%m.%y')
         return ddmmyy_date
-     except ValueError:
-        print("Неверный формат даты. Пожалуйста, укажите дату в формате ГГГГ-ММ-ДД.")
+    except ValueError:
+        print("Неверный формат даты. Пожалуйста, укажите дату в формате"
+            "ГГГГ-ММ-ДД.")
 
 
 
@@ -83,41 +87,59 @@ if len(data) != 0:
     # Заполнение таблицы roles
     if 'roles' in data:
         for role_s in data['roles']:
-            cursor.execute('SELECT name FROM roles WHERE name = ?', (data['roles'][role_s]['name'],))
+            cursor.execute('SELECT name FROM roles WHERE name = ?',
+                           (data['roles'][role_s]['name'],))
             existing_role = cursor.fetchone()
             if existing_role is None:
                 cursor.execute('''
-                    INSERT INTO roles (name, description, work_shift, lost_tag_flag)
+                    INSERT INTO roles (name, description, work_shift, 
+                               lost_tag_flag)
                     VALUES (?, ?, ?, ?)
-                ''', (data['roles'][role_s]['name'], data['roles'][role_s]['description'],
-                      data['roles'][role_s]['work_shift'], data['roles'][role_s]['lost_tag_flag']))
+                ''', (data['roles'][role_s]['name'], data['roles']
+                      [role_s]['description'],
+                      data['roles'][role_s]['work_shift'], data['roles']
+                      [role_s]['lost_tag_flag']))
                 conn.commit()
                 print('Коммит прошел успешно')
             else:
-                print(f"Role with name {data['roles'][role_s]['name']} already exists")
+                print(f"Role with name {data['roles'][role_s]['name']}"
+                      f"already exists")
 
     # Заполнение таблицы employees
     if 'employees' in data:
         for employee in data['employees']:
             # Проверка наличия записи с таким id
-            cursor.execute('SELECT id FROM employees WHERE id = ?', (employee['id'],))
+            cursor.execute(
+                'SELECT id FROM employees WHERE id = ?', 
+                (employee['id'],)
+                )
             existing_employee = cursor.fetchone()
             if existing_employee is None:
-                cursor.execute('SELECT id FROM roles WHERE name = ?', (employee['role'],))
+                cursor.execute(
+                    'SELECT id FROM roles WHERE name = ?', 
+                    (employee['role'],)
+                    )
                 existing_role = cursor.fetchone()
                 if existing_role is None:
                     print(f"Role '{employee['role']}' does not exist")
                 else:
                     cursor.execute('''
-                        INSERT INTO employees (id, first_name, last_name, role, hourly_rate, hire_date, birth_date)
+                        INSERT INTO employees (id, first_name, last_name, 
+                                   role, hourly_rate, hire_date, birth_date)
                         VALUES (?, ?, ?, ?, ?, ?, ?)
-                    ''', (employee['id'], employee['first_name'], employee['last_name'], existing_role[0],
-                          employee['hourly_rate'], employee['hire_date'], employee['birth_date']))
+                    ''', (employee['id'], employee['first_name'],
+                          employee['last_name'], existing_role[0],
+                          employee['hourly_rate'], employee['hire_date'],
+                          employee['birth_date'])
+                          )
                     conn.commit()
             else:
                 print(f"Employee with id {employee['id']} already exists")
                 # Обновление существующей записи по id
-                cursor.execute('SELECT id FROM employees WHERE id = ?', (employee['id'],))
+                cursor.execute(
+                    'SELECT id FROM employees WHERE id = ?', 
+                    (employee['id'],)
+                    )
                 existing_employee = cursor.fetchone()
                 if existing_employee is not None:
                     update_query = 'UPDATE employees SET '
@@ -129,7 +151,10 @@ if len(data) != 0:
                         update_query += 'last_name = ?, '
                         update_values.append(employee['last_name'])
                     if 'role' in employee:
-                        cursor.execute('SELECT id FROM roles WHERE name = ?', (employee['role'],))
+                        cursor.execute(
+                            'SELECT id FROM roles WHERE name = ?', 
+                            (employee['role'],)
+                            )
                         existing_role = cursor.fetchone()
                         if existing_role is not None:
                             update_query += 'role = ?, '
@@ -145,32 +170,31 @@ if len(data) != 0:
                     if 'birth_date' in employee:
                         update_query += 'birth_date = ?, '
                         update_values.append(employee['birth_date'])
-                    
                     # Remove the trailing comma and space from the update_query
                     update_query = update_query.rstrip(', ')
-                    
-                    # Add the WHERE clause to specify the id of the employee to update
+                    # Add the WHERE clause to specify
+                    #the id of the employee to update
                     update_query += ' WHERE id = ?'
                     update_values.append(employee['id'])
-                    
                     cursor.execute(update_query, update_values)
                     conn.commit()
-                    print(f"Employee with id {employee['id']} updated successfully")
+                    print(f"Employee with id {employee['id']}"
+                          f"updated successfully")
                 else:
                     print(f"Employee with id {employee['id']} does not exist")
 #Закрыть соединение
 conn.close()
-
 # Проверка заполнения таблиц
 conn = sqlite3.connect('company.db')
 cursor = conn.cursor()
-cursor.execute('''SELECT employees.id, employees.first_name, 
+cursor.execute('''SELECT employees.id, employees.first_name,
                employees.last_name, roles.name, employees.hourly_rate, 
                employees.hire_date, employees.birth_date FROM employees 
                INNER JOIN roles ON employees.role = roles.id''')
 rows = cursor.fetchall()
 for row in rows:
-    employee_id, first_name, last_name, role_name, hourly_rate, hire_date, birth_date = row
+    (employee_id, first_name, last_name, role_name, hourly_rate,
+     hire_date, birth_date) = row
     print((
         f"ID сотрудника: {employee_id}, "
         f"Имя: {first_name}, "
@@ -181,7 +205,7 @@ for row in rows:
         f"Дата рождения: {birth_date}"
     ))
 
-cursor.execute('''SELECT roles.id, roles.name, roles.description, 
+cursor.execute('''SELECT roles.id, roles.name, roles.description,
                roles.work_shift, roles.lost_tag_flag FROM roles''')
 rows = cursor.fetchall()
 for row in rows:
