dcc07f14e77a97ae0b1a4abd13f6013347ce3d9d
Thrawn1 <b.eremin@gmail.com>
2024-04-04 06:24:54 +0300

    Update tmp_3.py

--- tmp_3.py
+++ tmp_3.py
@@ -1,3 +1,4 @@
+import toml
 a = ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07',
       '2020-08', '2020-09', '2020-10', '2020-11', '2020-12']
 b = ['2020-01', '2021-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07',
@@ -5,6 +6,20 @@ b = ['2020-01', '2021-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07'
 
 c = set(a) - set(b)
 
-print(type(c))
-print(len(c))
-print(c)
+
+def screening_row(screen_condition:set,ame_file_data_label:str) -> list:
+    for condition in screen_condition:
+        year = condition[:4]
+        month = condition[5:]
+        print(f"Год: {year}, месяц: {month}")
+
+screening_row(c, '1_attlog.dat')
+
+with open('company_data_real copy.toml', 'r', encoding='utf-8') as file:
+    data = toml.load(file)
+
+
+
+print(data)
+# print(data['roles'])
+# print(data['employees'])
