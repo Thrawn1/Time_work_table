917ce4640477d7fad9673d0124413fc1867d36bc
Thrawn1 <b.eremin@gmail.com>
2025-02-26 19:22:30 +0300

    Create Workday.py

---
+++ Workday.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+
+class Workday():
+    def __init__(self, date:datetime.date, employee_id:int):
+        self.employee_id = employee_id
+        self.start_time = None
+        self.end_time = None
+        self.date = date
+        self.is_weekend = False
+        self.is_vacation = False
+        self.is_sick_leave = False
+        self.raw_line = None
+
+
+
+    def check_missing_mark(self):
+        if self.start_time is None or self.end_time is None:
+            return True
+    def get_workday_hours(self):
+        if self.start_time is None or self.end_time is None:
+            return 0
+        return (self.end_time - self.start_time).seconds / 3600
+    def __str__(self):
+        return f"Workday: {self.date} {self.time} {self.employee_id}"
